ae9f2003a3b2ddec7b8324de76c41d2a69752c77
jassem-razzaq <jassem.razzaq@gmail.com>
2025-11-15 19:04:33 -0500

    search podcast still incomplete

--- hearsay_procedures.sql
+++ hearsay_procedures.sql
@@ -25,8 +25,21 @@ DROP PROCEDURE IF EXISTS get_user;
 DELIMITER $$
 CREATE PROCEDURE get_user
 (
+	user_id INT
+)
+BEGIN
+	SELECT * FROM user WHERE id = user_id;
+END $$
+DELIMITER ;
+
+-- -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Log in user
+DROP PROCEDURE IF EXISTS log_in_user;
+DELIMITER $$
+CREATE PROCEDURE log_in_user
+(
 	username VARCHAR(32),
-    password_hash VARCHAR(32)
+    password_hash VARCHAR(255)
 )
 BEGIN
 	SELECT id FROM user WHERE user.email = email AND user.password_hash = password_hash;
@@ -97,8 +110,10 @@ BEGIN
     DECLARE genre_flag TINYINT DEFAULT 0;
     DECLARE language_flag TINYINT DEFAULT 0;
     DECLARE platform_flag TINYINT DEFAULT 0;
-    DECLARE host_flag TINYINT DEFAULT 0;
-    DECLARE guest_flag TINYINT DEFAULT 0;
+    DECLARE host_first_flag TINYINT DEFAULT 0;
+    DECLARE host_last_flag TINYINT DEFAULT 0;
+    DECLARE guest_first_flag TINYINT DEFAULT 0;
+    DECLARE guest_last_flag TINYINT DEFAULT 0;
     DECLARE year_flag TINYINT DEFAULT 0;
     
     -- Set flags to disable check if filter is null
@@ -106,18 +121,25 @@ BEGIN
 	IF genre IS NULL THEN SET genre_flag = 1; END IF;
 	IF language IS NULL THEN SET language_flag = 1; END IF;
 	IF platform IS NULL THEN SET platform_flag = 1; END IF;
-	IF host_first IS NULL AND host_last IS NULL THEN SET host_flag = 1; END IF;
-	IF guest_first IS NULL AND guest_last IS NULL THEN SET guest_flag = 1; END IF;
+	IF host_first IS NULL THEN SET host_first_flag = 1; END IF;
+	IF host_last IS NULL THEN SET host_last_flag = 1; END IF;
+	IF guest_first IS NULL THEN SET guest_first_flag = 1; END IF;
+	IF guest_last IS NULL THEN SET guest_last_flag = 1; END IF;
 	IF year IS NULL THEN SET year_flag = 1; END IF;
 	
     -- Return table based on passed filters
-	SELECT * FROM podcast AS p
+    SELECT * FROM podcast AS p JOIN platform_to_podcast AS ptp ON p.id = ptp.podcast_id
+						   JOIN platform AS pl ON ptp.platform_name = pl.name
+                           JOIN genre_to_podcast AS gtp ON p.id = gtp.podcast_id
+                           JOIN genre AS g ON gtp.genre_name = g.name
+                           JOIN language_to_podcast AS ltp ON p.id = ltp.podcast_id
+                           JOIN language AS l ON ltp.language_name = l.name
+                           JOIN 
+	WITH H AS (SELECT * FROM podcast JOIN 
     WHERE (p.name = name OR name_flag) AND
 		  (p.genre = genre OR genre_flag) AND
           (p.language = language OR language_flag) AND
           (p.platform = platform OR platform_flag) AND
-          (p.host_first = name OR name_flag) AND
-          (p.name = name OR name_flag) AND
-          (p.name = name OR name_flag);
+          (p.host_first = host_first OR host_first_flag) AND
 END $$
 DELIMITER ;
