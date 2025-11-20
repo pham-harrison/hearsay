25058cc7adfc9ea1058c4f389a56c2f4677b8f59
Harrison Pham <hpham1997@gmail.com>
2025-11-20 16:58:23 -0500

    add endpoint to get podcast filters

--- backend/routers/podcasts.py
+++ backend/routers/podcasts.py
@@ -6,15 +6,18 @@ from .episodes import router as episodes_router
 from .reviews import router as reviews_router
 import pymysql
 
+
 class Review(BaseModel):
     rating: int
     comment: str
+
 
 router = APIRouter(prefix="/podcasts")
 router.include_router(episodes_router, prefix="/{podcast_id}/episodes")
 router.include_router(reviews_router, prefix="/{podcast_id}/reviews")
 
-# Search for a podcast 
+
+# Search for a podcast
 @router.get("/")
 async def searchPodcasts(
     name: Optional[str] = None,
@@ -23,7 +26,7 @@ async def searchPodcasts(
     platform: Optional[str] = None,
     host: Optional[str] = None,
     guest: Optional[str] = None,
-    year: Optional[int] = None
+    year: Optional[int] = None,
 ):
     try:
         with db_cursor() as cursor:
@@ -35,38 +38,99 @@ async def searchPodcasts(
             guest_first_name, guest_last_name = None, None
             if guest:
                 guest_name_parts = guest.split()
-                guest_first_name, guest_last_name = guest_name_parts[0], guest_name_parts[1]
-                
-            cursor.callproc("search_podcasts", (name, genre, language, platform, host_first_name, host_last_name, guest_first_name, guest_last_name, year))
+                guest_first_name, guest_last_name = (
+                    guest_name_parts[0],
+                    guest_name_parts[1],
+                )
+
+            cursor.callproc(
+                "search_podcasts",
+                (
+                    name,
+                    genre,
+                    language,
+                    platform,
+                    host_first_name,
+                    host_last_name,
+                    guest_first_name,
+                    guest_last_name,
+                    year,
+                ),
+            )
             filtered_podcasts = cursor.fetchall()
             return filtered_podcasts
     except pymysql.err.OperationalError as e:
         error_code, message = e.args
         raise HTTPException(status_code=400, detail=message)
 
-# Get all hosts
-@router.get("/hosts")
-async def getHosts():
+
+# # Get all hosts
+# @router.get("/hosts")
+# async def getHosts():
+#     try:
+#         with db_cursor() as cursor:
+#             cursor.callproc("get_hosts")
+#             all_hosts = cursor.fetchall()
+#             return all_hosts
+#     except pymysql.err.OperationalError as e:
+#         error_code, message = e.args
+#         raise HTTPException(status_code=400, detail=message)
+
+# # Get all guests
+# @router.get("/guests")
+# async def getHosts():
+#     try:
+#         with db_cursor() as cursor:
+#             cursor.callproc("get_guests")
+#             all_guests = cursor.fetchall()
+#             return all_guests
+#     except pymysql.err.OperationalError as e:
+#         error_code, message = e.args
+#         raise HTTPException(status_code=400, detail=message)
+
+
+@router.get("/filters")
+async def getFilters():
     try:
         with db_cursor() as cursor:
-            cursor.callproc("get_hosts")
-            all_hosts = cursor.fetchall()
-            return all_hosts
+            filters = {}
+
+            cursor.execute("SELECT genre_name from genre;")
+            genre_rows = cursor.fetchall()
+            genres = [f"{row["genre_name"]}" for row in genre_rows]
+            filters["genres"] = genres
+
+            cursor.execute("SELECT language_name from language;")
+            language_rows = cursor.fetchall()
+            languages = [f"{row["language_name"]}" for row in language_rows]
+            filters["languages"] = languages
+
+            cursor.execute("SELECT platform_name from platform;")
+            platform_rows = cursor.fetchall()
+            platforms = [f"{row["platform_name"]}" for row in platform_rows]
+            filters["platforms"] = platforms
+
+            cursor.execute("SELECT DISTINCT YEAR(release_date) as year from podcast;")
+            year_rows = cursor.fetchall()
+            years = [f"{row["year"]}" for row in year_rows]
+            years.sort(reverse=True)
+            filters["years"] = years
+
+            cursor.execute("SELECT first_name, last_name from host")
+            host_rows = cursor.fetchall()
+            hosts = [f"{row['first_name']} {row['last_name']}" for row in host_rows]
+            filters["hosts"] = hosts
+
+            cursor.execute("SELECT first_name, last_name from guest")
+            guest_rows = cursor.fetchall()
+            guests = [f"{row["first_name"]} {row["last_name"]}" for row in guest_rows]
+            filters["guests"] = guests
+
+            return filters
     except pymysql.err.OperationalError as e:
         error_code, message = e.args
         raise HTTPException(status_code=400, detail=message)
 
-# Get all guests
-@router.get("/guests")
-async def getHosts():
-    try:
-        with db_cursor() as cursor:
-            cursor.callproc("get_guests")
-            all_guests = cursor.fetchall()
-            return all_guests
-    except pymysql.err.OperationalError as e:
-        error_code, message = e.args
-        raise HTTPException(status_code=400, detail=message)
 
 # Get all derived podcast ratings for a user
 @router.get("/{podcast_id}/ratings/{user_id}")
@@ -80,18 +144,32 @@ async def getPodcastDerivedRatings(podcast_id: int, user_id: int):
             stmt = "SELECT get_global_podcast_avg_rating_by_episode(%s)"
             cursor.execute(stmt, (podcast_id,))
             global_avg_rating_by_ep = cursor.fetchone()
-            
+
             stmt = "SELECT get_user_friends_podcast_avg_rating(%s, %s)"
-            cursor.execute(stmt, (user_id, podcast_id,))
+            cursor.execute(
+                stmt,
+                (
+                    user_id,
+                    podcast_id,
+                ),
+            )
             friends_avg_rating = cursor.fetchone()
 
             stmt = "SELECT get_user_friends_podcast_avg_rating_by_episode(%s, %s)"
-            cursor.execute(stmt, (user_id, podcast_id,))
+            cursor.execute(
+                stmt,
+                (
+                    user_id,
+                    podcast_id,
+                ),
+            )
             friends_avg_rating_by_ep = cursor.fetchone()
-            return {"global_avg_rating": list(global_avg_rating.values())[0],
-                    "global_avg_rating_by_ep": list(global_avg_rating_by_ep.values())[0],
-                    "friends_avg_rating": list(friends_avg_rating.values())[0],
-                    "friends_avg_rating_by_ep": list(friends_avg_rating_by_ep.values())[0]}
+            return {
+                "global_avg_rating": list(global_avg_rating.values())[0],
+                "global_avg_rating_by_ep": list(global_avg_rating_by_ep.values())[0],
+                "friends_avg_rating": list(friends_avg_rating.values())[0],
+                "friends_avg_rating_by_ep": list(friends_avg_rating_by_ep.values())[0],
+            }
     except pymysql.err.OperationalError as e:
         error_code, message = e.args
         raise HTTPException(status_code=400, detail=message)
