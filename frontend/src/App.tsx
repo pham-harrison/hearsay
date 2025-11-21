bfc75890cbe6bb068181d1de96559986aeb6f397
Harrison Pham <hpham1997@gmail.com>
2025-11-20 22:27:11 -0500

    create router layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Home from "./pages/Home";
 
 function App() {
 
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
