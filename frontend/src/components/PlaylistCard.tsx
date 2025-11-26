d52b4473a22525c36ebb83df4e3ff68029330a8b
jassem-razzaq <jassem.razzaq@gmail.com>
2025-11-26 02:34:00 -0500

    fix: remove extra imports

--- frontend/src/components/PlaylistCard.tsx
+++ frontend/src/components/PlaylistCard.tsx
@@ -1,7 +1,3 @@
-import { useContext } from "react";
-import { useParams } from "react-router-dom";
-import { LoginContext } from "../contexts/LoginContext";
-
 type PlaylistCardProps = {
   name: string;
   description: string;
@@ -13,11 +9,7 @@ export default function PlaylistCard({
   name,
   description,
   onClick,
-  onDelete,
 }: PlaylistCardProps) {
-  const { loggedIn, userID } = useContext(LoginContext);
-  const urlID = useParams().userID;
-
   return (
     <div className="bg-green-300 cursor-pointer" onClick={onClick}>
       {name} : {description}{" "}
